07d107fcf8998a2d1775f9b42015cc8c2d178bed
William Ross <241028570+paquinas@users.noreply.github.com>
2025-12-27 00:07:06 -0500

    Test new API

--- src/main.jl
+++ src/main.jl
@@ -2,22 +2,12 @@ using SimpleDirectMediaLayer
 using SimpleDirectMediaLayer.LibSDL2
 
 using Phoenix  # must be in the active environment
-SDL_Init(SDL_INIT_EVERYTHING)
-window = Frame(Vector2(500, 500), Vector2(450, 350), "Window!!!")
+Phoenix.Engine.Init("Phoenix", Phoenix.Types.Vector2(25,25), Phoenix.Types.Vector2(1000,1000))
 
-global _run = true
-while _run
-    eventRef = Ref{SDL_Event}()
-    while Bool(SDL_PollEvent(eventRef))
-        if eventRef[].type == SDL_QUIT
-            global _run = false
-            break
-        end
-    end
+Phoenix.Draw.Line(Phoenix.Vector2(0,500), Phoenix.Vector2(500,0), Phoenix.Color("#77777777"))
 
-    SDL_Delay(1000 ÷ 60)
-end
+Phoenix.Screen.Display()
 
-SDL_DestroyRenderer(window.renderer)
-SDL_DestroyWindow(window.window)
-SDL_Quit()
+sleep(10)
+
+Phoenix.Engine.Quit()
